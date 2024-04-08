5ce8541ed188ae7aa21593c1f33aad6ab8870c4b
Yury Bursian <dab180996@gmail.com>
2024-04-08 20:35:02 +0300

    createUser error handling

--- controllers/users.js
+++ controllers/users.js
@@ -20,6 +20,9 @@ const createUser = (req, res) => {
     .then((user) => res.status(201).send(user))
     .catch((err) => {
       console.error(err);
+      if (err.name === "ValidationError") {
+        return res.status(400).send({ message: err.message });
+      }
       return res.status(500).send({ message: err.message });
     });
 };
