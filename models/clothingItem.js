fc70a8e5b83202643c08a3df9f3a454bee870f25
Yury Bursian <dab180996@gmail.com>
2024-04-09 14:05:17 +0300

    clothingItem schema

--- models/clothingItem.js
+++ models/clothingItem.js
@@ -1,5 +1,34 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
-const clothingItemSchema = new mongoose.Schema({});
+const clothingItemSchema = new mongoose.Schema({
+  name: { type: String, required: true, minlength: 2, maxlength: 30 },
+  weather: { type: String, required: true, enum: ["hot", "warm", "cold"] },
+  imageUrl: {
+    type: String,
+    required: [true, "The image URL field is required"],
+    validate: {
+      validator(value) {
+        return validator.isURL(value);
+      },
+      message: "Please enter a valid URL",
+    },
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  likes: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
 module.exports = mongoose.model("clothing", clothingItemSchema);
