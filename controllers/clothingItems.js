8ef62a520d3982d251fc766ccdf3bbcb748cfa88
Yury Bursian <dab180996@gmail.com>
2024-04-09 15:18:18 +0300

    typo fix

--- controllers/clothingItems.js
+++ controllers/clothingItems.js
@@ -1,13 +1,22 @@
 const ClothingItem = require("../models/clothingItem");
 const { ERROR_CODES } = require("../utils/errors");
 
+const getItems = (req, res) => {
+  ClothingItem.find({})
+    .then((items) => res.status(200).send(items))
+    .catch((err) => {
+      console.error(err.name);
+      return res
+        .status(ERROR_CODES.INTERNAL_SERVER_ERROR)
+        .send({ message: err.message });
+    });
+};
+
 const createItem = (req, res) => {
   const { name, weather, imageUrl } = req.body;
 
   ClothingItem.create({ name, weather, imageUrl, owner: req.user._id })
-    .then((item) => {
-      res.status(200).send({ data: item });
-    })
+    .then((item) => res.status(200).send({ data: item }))
     .catch((err) => {
       console.error(err.name);
       if (err.name === "ValidationError") {
@@ -21,40 +30,4 @@ const createItem = (req, res) => {
     });
 };
 
-const getItems = (req, res) => {
-  ClothingItem.find({})
-    .then((items) => res.status(200).send(items))
-    .catch((err) => {
-      console.error(err.name);
-      return res
-        .status(ERROR_CODES.INTERNAL_SERVER_ERROR)
-        .send({ message: err.message });
-    });
-};
-
-const deleteItem = (req, res) => {
-  const { itemId } = req.params;
-  console.log("Deleting item with ID:", itemId);
-
-  ClothingItem.findByIdAndDelete(itemId)
-    .orFail()
-    .then((item) => {
-      console.log(item);
-      res.status(200).send({});
-    })
-    .catch((err) => {
-      console.log(err.name);
-      if (err.name === "CastError") {
-        return res
-          .status(ERROR_CODES.BAD_REQUEST)
-          .send({ message: err.message });
-      } else if (err.name === "DocumentNotFoundError") {
-        return res.status(ERROR_CODES.NOT_FOUND).send({ message: err.message });
-      }
-      return res
-        .status(ERROR_CODES.INTERNAL_SERVER_ERROR)
-        .send({ message: err.message });
-    });
-};
-
-module.exports = { createItem, getItems, deleteItem };
+module.exports = { createItem, getItems };
--- routes/clothingItems.js
+++ routes/clothingItems.js
@@ -1,13 +1,9 @@
 const router = require("express").Router();
 
-const {
-  createItem,
-  getItems,
-  deleteItem,
-} = require("../controllers/clothingItems");
+const { createItem, getItems } = require("../controllers/clothingItems");
 
 router.post("/", createItem);
 router.get("/", getItems);
-router.delete("/:itemId", deleteItem);
+// router.delete("/:itemId", deleteItem);
 
 module.exports = router;
