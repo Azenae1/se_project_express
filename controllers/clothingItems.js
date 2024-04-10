16002706bca0d1a108c75c9f58664c1161d87a38
Yury Bursian <dab180996@gmail.com>
2024-04-10 20:15:03 +0300

    dislikeItem&likeItem tested

--- controllers/clothingItems.js
+++ controllers/clothingItems.js
@@ -30,15 +30,45 @@ const createItem = (req, res) => {
     });
 };
 
-const likeItem = (req, res) => {
+// const likeItem = (req, res) => {
+//   const { itemId } = req.params;
+//   ClothingItem.findByIdAndUpdate(
+//     itemId,
+//     { $addToSet: { likes: req.user._id } },
+//     { new: true },
+//   )
+//     .then((item) => {
+//       res.status(200).send({ message: "Item liked" });
+//     })
+//     .catch((err) => {
+//       console.error(err);
+//       if (err.name === "DocumentNotFoundError") {
+//         return res.status(ERROR_CODES.NOT_FOUND).send({ message: err.message });
+//       }
+//       if (err.name === "CastError") {
+//         return res
+//           .status(ERROR_CODES.BAD_REQUEST)
+//           .send({ message: err.message });
+//       }
+//       return res
+//         .status(ERROR_CODES.INTERNAL_SERVER_ERROR)
+//         .send({ message: err.message });
+//     });
+// };
+
+const dislikeItem = (req, res) => {
   const { itemId } = req.params;
-  ClothingItem.findByIdAndUpdate(
-    itemId,
-    { $addToSet: { likes: req.user._id } },
-    { new: true },
-  )
+  ClothingItem.findById(itemId)
+    .orFail()
     .then((item) => {
-      res.status(200).send({ message: "Item liked" });
+      if (!item.likes.includes(req.user._id)) {
+        return res.status(200).send({ message: "User hasn't liked this item" });
+      }
+      item.likes.pull(req.user._id);
+      return item.save();
+    })
+    .then((updatedItem) => {
+      res.status(200).send({ message: "Item disliked", item: updatedItem });
     })
     .catch((err) => {
       console.error(err);
@@ -55,7 +85,40 @@ const likeItem = (req, res) => {
         .send({ message: err.message });
     });
 };
+
+const likeItem = (req, res) => {
+  const { itemId } = req.params;
+  ClothingItem.findById(itemId)
+    .orFail()
+    .then((item) => {
+      if (item.likes.includes(req.user._id)) {
+        return res
+          .status(200)
+          .send({ message: "User has already liked this item" });
+      }
+      item.likes.push(req.user._id);
+      return item.save();
+    })
+    .then((updatedItem) => {
+      res.status(200).send({ message: "Item liked", item: updatedItem });
+    })
+    .catch((err) => {
+      console.error(err);
+      if (err.name === "DocumentNotFoundError") {
+        return res.status(ERROR_CODES.NOT_FOUND).send({ message: err.message });
+      }
+      if (err.name === "CastError") {
+        return res
+          .status(ERROR_CODES.BAD_REQUEST)
+          .send({ message: err.message });
+      }
+      return res
+        .status(ERROR_CODES.INTERNAL_SERVER_ERROR)
+        .send({ message: err.message });
+    });
+};
+
 module.exports.createClothingItem = (req, res) => {
   console.log(req.user._id);
 };
-module.exports = { createItem, getItems, likeItem };
+module.exports = { createItem, getItems, likeItem, dislikeItem };
--- routes/clothingItems.js
+++ routes/clothingItems.js
@@ -4,12 +4,13 @@ const {
   createItem,
   getItems,
   likeItem,
+  dislikeItem,
 } = require("../controllers/clothingItems");
 
 router.post("/", createItem);
 router.get("/", getItems);
 router.put("/:itemId/likes", likeItem);
-// router.delete("/:itemId/likes", dislikeItem);
+router.delete("/:itemId/likes", dislikeItem);
 // router.delete("/:itemId", deleteItem);
 
 module.exports = router;
