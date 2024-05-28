0557ece815cba67623a3b650eefced4e81e1c4c5
Yury Bursian <dab180996@gmail.com>
2024-05-28 15:55:27 +0300

    error handling fix

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,17 +1,17 @@
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const { JWT_SECRET } = require("../utils/config");
-const { AUTH_ERROR } = require("../utils/errors");
+const { AuthError } = require("../utils/err_auth");
 
 const authorization = (req, res, next) => {
   const { authorization: auth } = req.headers;
   if (!auth || !auth.startsWith("Bearer ")) {
-    return res.status(AUTH_ERROR).json({ message: "The session is expired" });
+    return next(new AuthError("The session is expired"));
   }
   const token = auth.replace("Bearer ", "");
   return jwt.verify(token, JWT_SECRET, (err, payload) => {
     if (err) {
-      return res.status(AUTH_ERROR).json({ message: "Authorization required" });
+      return next(new AuthError("Authorization required"));
     }
     req.user = payload;
 
